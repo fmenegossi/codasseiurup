df63e5df7c7f757bdf0a47beb68ad0c5851f4214
Felipe Menegossi <felipe.menegossi@gmail.com>
2018-01-16 11:24:33 +0100

    One-to-many: Models

--- Gemfile
+++ Gemfile
@@ -44,6 +44,14 @@ gem 'devise'
 gem "font-awesome-rails"
 
 group :development, :test do
+  gem 'rspec-rails', '~> 3.5', '>= 3.5.2'
+end
+
+group :test do
+  gem 'capybara', '~> 2.9', '>= 2.9.1'
+end
+
+group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+	devise :database_authenticatable, :registerable,
+		:recoverable, :rememberable, :trackable, :validatable
+
+	has_many :events, dependent: :destroy
+
+	validates :name, presence: true
+	validates :description, presence: true, length: { maximum: 500 }
 end
