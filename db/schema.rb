5bc2e981c68b36ce9b57ee92bbca21f96ada7813
Felipe Menegossi <felipe.menegossi@gmail.com>
2018-01-18 13:02:39 +0100

    Cloudinary stuff

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180116164257) do
+ActiveRecord::Schema.define(version: 20180118110637) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,14 @@ ActiveRecord::Schema.define(version: 20180116164257) do
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
+  create_table "photos", force: :cascade do |t|
+    t.bigint "event_id"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_photos_on_event_id"
+  end
+
   create_table "profiles", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -71,5 +79,6 @@ ActiveRecord::Schema.define(version: 20180116164257) do
   end
 
   add_foreign_key "events", "users"
+  add_foreign_key "photos", "events"
   add_foreign_key "profiles", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,34 @@
-User.destroy_all
 Category.destroy_all
+Photo.destroy_all
+Event.destroy_all
+Profile.destroy_all
+User.destroy_all
 
-user1 = User.create!(email: 'felipe.menegossi@gmail.com', password: 'abc123')
-user2 = User.create!(email: 'felipe@gmail.com', password: 'abc123')
+# Users
+user1 = User.create!(email: 'felipe.menegossi@gmail.com', password: 'abcd1234')
+user2 = User.create!(email: 'felipe@gmail.com', password: 'abcd1234')
 miriam = User.create(email: "miriam@codaisseurup.com", password: "abcd1234")
 
-Event.create!(
-	name: "Just an poor event.",
-	description: "That's just a description of this event.",
-	price: 15,
-	capacity: 10,
-	starts_at: Time.now,
-	ends_at: Time.now + 1.days,
-	user: user1
-)
+# Events
+event1 = Event.create!(name: "Just an poor event.", description: "That's just a description of this event.",
+				   price: 15, capacity: 10, starts_at: Time.now,	ends_at: Time.now + 2.days,	user: user1)
+event2 = Event.create!(name: "Test event 2", description: "Craziest event you ever know.",
+				   price: 15, capacity: 10, starts_at: Time.now,	ends_at: Time.now + 6.days,	user: user2)
+event3 = Event.create!(name: "Just testing events", description: "It isn't an event!",
+				   price: 15, capacity: 10, starts_at: Time.now,	ends_at: Time.now + 4.days,	user: user1)
 
-
-Event.create!(name: "Monthly Yoga Weekend", description: "Let's come together and practice some asanas together in the woods!", location: "Amsterdam", includes_food: false, includes_drinks: true, price: 10.00, starts_at: 10.days.from_now, ends_at: 12.days.from_now, capacity: 100, active: true,  user: miriam)
-
+# Categories
 Category.create!([{name:"+18 Only"},{name:"Matine"},{name:"Pizza"},{name:"Happy Hour"},])
+
+# Photos
+photo1 = Photo.create!(remote_image_url: "http://res.cloudinary.com/fmenegossi/image/upload/v1516274323/kevin-364843_fpfspl.jpg", event: event1)
+photo2 = Photo.create!(remote_image_url: "http://res.cloudinary.com/fmenegossi/image/upload/v1516274323/ilya-pavlov-87438_ld37au.jpg", event: event1)
+photo3 = Photo.create!(remote_image_url: "http://res.cloudinary.com/fmenegossi/image/upload/v1516274323/kevin-364843_fpfspl.jpg", event: event1)
+
+photo4 = Photo.create!(remote_image_url: "http://res.cloudinary.com/fmenegossi/image/upload/v1516274323/ilya-pavlov-87438_ld37au.jpg", event: event2)
+photo5 = Photo.create!(remote_image_url: "http://res.cloudinary.com/fmenegossi/image/upload/v1516274323/kevin-364843_fpfspl.jpg", event: event2)
+photo6 = Photo.create!(remote_image_url: "http://res.cloudinary.com/fmenegossi/image/upload/v1516274323/ilya-pavlov-87438_ld37au.jpg", event: event2)
+
+photo7 = Photo.create!(remote_image_url: "http://res.cloudinary.com/fmenegossi/image/upload/v1516274323/ilya-pavlov-87438_ld37au.jpg", event: event3)
+photo8 = Photo.create!(remote_image_url: "http://res.cloudinary.com/fmenegossi/image/upload/v1516274323/ilya-pavlov-87438_ld37au.jpg", event: event3)
+photo9 = Photo.create!(remote_image_url: "http://res.cloudinary.com/fmenegossi/image/upload/v1516274323/kevin-364843_fpfspl.jpg", event: event3)
